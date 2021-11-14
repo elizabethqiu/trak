6f40d0fd0c4b8a8c2aad359b7950a0eb742290f0
Elizabeth Qiu <elizabethjqiu@gmail.com>
2021-11-14 12:15:29 -0500

    FLASKKKKKK 🥰

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request
+from send_sms import *
 
 app = Flask(__name__)
 
@@ -7,6 +9,29 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def update_record():
+    # print(request.data)
+    print(request.form)
+    name = request.form['name']
+    phone_number = request.form['phone_number']
+    text_user(phone_number)
+    return render_template('screen2.html')
+    # record = json.loads(request.data)
+    # new_records = []
+    # with open('/tmp/data.txt', 'r') as f:
+    #     data = f.read()
+    #     records = json.loads(data)
+    # for r in records:
+    #     if r['name'] == record['name']:
+    #         r['phone_number'] = record['phone_number']
+    #     new_records.append(r)
+    # with open('/tmp/data.txt', 'w') as f:
+    #     f.write(json.dumps(new_records, indent=2))
+    # return jsonify(record)
+    # print(record)
 
 
 # Run web server and app
